b066c6b21dc6ecd963c89ca2d4fd21c70cc23207
Adeola Oladeji <deolaoladeji@gmail.com>
2022-09-06 09:38:53 +0100

    Adding swaggerUI config

--- src/main/java/com/deedeji/ecommerce/config/SwaggerConfig.java
+++ src/main/java/com/deedeji/ecommerce/config/SwaggerConfig.java
@@ -1,8 +1,54 @@
 package com.deedeji.ecommerce.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Configuration
-
+@EnableSwagger2
+@EnableWebMvc
+@Component
 public class SwaggerConfig {
+
+    @Bean
+	public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.deedeji.ecommerce"))
+            .paths(PathSelectors.ant("/api/*"))
+            .build()
+//            .directModelSubstitute(LocalDate.class, java.sql.Date.class)
+//            .directModelSubstitute(LocalDateTime.class, java.util.Date.class)
+            .apiInfo(apiInfo());
+        }
+        	private ApiInfo apiInfo() {
+                Contact contact = new Contact("adeola-ecommerce", "", "deolaoladeji@gmail.com");
+        		return new ApiInfoBuilder()
+        			.title("Ëcommerce API")
+        			.description("DOcumentation of Ecommerce api")
+        			.version("1.0.0")
+        			.contact(contact)
+        			.build();
+        	}
+
+        /**
+     		...
+     		...
+     		Getters
+     		Setters
+     		...
+     		...
+     	**/
 }
--- src/main/java/com/deedeji/ecommerce/controller/CategoryController.java
+++ src/main/java/com/deedeji/ecommerce/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.deedeji.ecommerce.controller;
 
 import com.deedeji.ecommerce.data.models.Category;
 import com.deedeji.ecommerce.services.CategoryService;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/category")
+@ApiResponses(value = {
+        @io.swagger.annotations.ApiResponse(code = 400, message = "This is a bad request, please follow the API documentation for the proper request format"),
+        @io.swagger.annotations.ApiResponse(code = 401, message = "Due to security constraints, your access request cannot be authorized"),
+        @io.swagger.annotations.ApiResponse(code = 500, message = "The server is down. Please bear with us."),
+})
+
 public class CategoryController {
 
     @Autowired
