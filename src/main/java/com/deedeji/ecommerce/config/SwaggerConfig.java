02c321277a122359da2470d21f3dca0182e802a9
Adeola Oladeji <deolaoladeji@gmail.com>
2022-09-06 10:33:26 +0100

    setting up swagger-ui configuration

--- src/main/java/com/deedeji/ecommerce/config/SwaggerConfig.java
+++ src/main/java/com/deedeji/ecommerce/config/SwaggerConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,17 +15,16 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Configuration
-@EnableSwagger2WebMvc
+//@EnableSwagger2WebMvc
 @EnableSwagger2
-@EnableWebMvc
+//@EnableWebMvc
 @Component
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
 	public Docket api() {
@@ -46,12 +47,14 @@ public class SwaggerConfig {
         			.build();
         	}
 
-	/**
-     		...
-     		...
-     		Getters
-     		Setters
-     		...
-     		...
-     	**/
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/configuration/ui", "/swagger-resources/configuration/ui");
+		}
+
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+			registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		}
 }
--- src/main/java/com/deedeji/ecommerce/config/WebConfig.java
+++ src/main/java/com/deedeji/ecommerce/config/WebConfig.java
@@ -3,8 +3,10 @@ package com.deedeji.ecommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 class WebConfig extends WebMvcConfigurationSupport {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -15,4 +17,5 @@ class WebConfig extends WebMvcConfigurationSupport {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }}
+    }
+}
